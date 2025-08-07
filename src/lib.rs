87590a657c715558a7dbc05d2950b79850da7d25
Emerson Coskey <emerson@coskey.dev>
2025-08-06 23:01:13 -0700

    avoid lib.rs conflicts

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ pub struct VanePlugins;
 
 impl PluginGroup for VanePlugins {
     fn build(self) -> PluginGroupBuilder {
-        PluginGroupBuilder::start::<Self>()
+        let mut plugin_group = PluginGroupBuilder::start::<Self>();
+        plugin_group
     }
 }
